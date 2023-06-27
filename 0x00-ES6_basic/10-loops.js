5e5326686d6b92c9d730d610b5fe6d9b842067e6
lakelexy <alao8380@yahoo.com>
2023-06-27 12:35:07 +0000

    task10

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const modifiedArray = [];
+
+  for (const value of array) {
+    modifiedArray.push(appendString + value);
+  }
+
+  return modifiedArray;
+}
