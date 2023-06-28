0d74a81273e6ca4f9b2885f798bf80d8ac75643d
lakelexy <alao8380@yahoo.com>
2023-06-28 14:35:55 +0000

    task3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,13 @@
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
-export default function handleSignUp() {
-  Promise.all([uploadPhoto(), createUser()]).then(
-    (results) => {
+export default function handleProfileSignup() {
+  const data1 = uploadPhoto();
+  const data2 = createUser();
+  return Promise.all([data1, data2])
+    .then((results) => {
       console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`);
-    }, () => {
+    })
+    .catch(() => {
       console.log('Signup system offline');
-    },
-  );
+    });
 }
